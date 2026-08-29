81a3bc11c590af3a6ec242d78fc6e297271d0768
HarshMishra111 <harshumishra1112@gmail.com>
2026-08-29 20:02:55 +0530

    First  commit

---
+++ alb.tf
@@ -0,0 +1,62 @@
+resource "aws_lb" "frontend" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb.id
+  ]
+
+  subnets = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+# target group 
+
+resource "aws_lb_target_group" "app" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${var.project_name}-target-group"
+  }
+}
+
+#attach ec2
+
+resource "aws_lb_target_group_attachment" "app" {
+  count = 2
+
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = aws_instance.app[count.index].id
+  port             = 80
+}
+
+# HTTP listener
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.frontend.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
