21d03880de5d4d87abd17ed448f42f5b1bc99441
Harsh Mishra <124067526+HarshMishra111@users.noreply.github.com>
2026-08-30 19:05:42 +0530

    replacement in progress

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "data-storage-bucket-myapp"
-    key          = "terraform-webapp/terraform.tfstate"
-    region       = "eu-west-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
